2c3d1214fe4233a6094aad83a88ec7c3253a9fcd
Benjamin Wainer <benjawainer@gmail.com>
2023-06-26 22:40:04 +0900

    Moved logic to controller. It gives correct results for words not on the grid or if not in English.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,30 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @letters = (1..10).map{('A'..'Z').to_a[rand(26)]}
+    @letters = (1..10).map{ ('A'..'Z').to_a[rand(26)] }
   end
 
   def score
-    raise
+    @word = params[:word].upcase
+    @letters = params[:letters].split(' ')
+    # raise
+    if valid?(@word, @letters) == false
+      @result = "Sorry but <strong>#{@word}</strong> can't be built out of #{@letters.join(', ')}"
+    elsif english?(@word)
+      @result = "Sorry but <strong>#{@word}</strong> does not seem to be a valid English word..."
+    end
+    # @valid_english = english?(@word)
+  end
+
+  def valid?(word, letters)
+    word.chars.all? { |letter| word.chars.count(letter) <= letters.count(letter) }
+  end
+
+  def english?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    word_serialized = URI.open(url).read
+    word_data = JSON.parse(word_serialized)
+    word_data["found"]
   end
 end
